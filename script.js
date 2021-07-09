d384baaeedf6024c2b5b1a4be51db45582df11b2
liwll <liwill7778@gmail.com>
2021-07-09 13:19:34 -0400

    Form now adds an item to library and logs to console. Cleaned up UI a bit.

--- script.js
+++ script.js
@@ -13,26 +13,39 @@ let book = new Book("The Hobbit", "J.r.r tolkien", 295, false);
 let myLibrary = [];
 
 const modal = document.querySelector('.modal');
-const add = document.querySelector('.add-book');
-add.onclick = () => {
-    openModal();
+const form = document.querySelector('form');
+
+const newBook = document.querySelector('.new-book');
+newBook.onclick = () => {
+    modal.style.display = "block";
 };
 const closeModal = document.querySelector('.close-modal')
 closeModal.addEventListener('click', () => {
     modal.style.display = "none";
+    clearFormData();
 });
-window.onclick = (event) => {
+window.onmousedown = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
+        clearFormData();
     }
 }
+const submitForm = document.querySelector('.submit');
+submitForm.addEventListener('click', () => {
+    addBookToLibrary();
+    clearFormData();
+});
 
-function openModal() {
-    const modal = document.querySelector('.modal');
-    modal.style.display = "block";
+function clearFormData() {
+    form.title.value = '';
+    form.author.value = '';
+    form.pages.value = '';
+    form.isRead.value = 'off';
+    modal.style.display = "none";
 }
 
 function addBookToLibrary() {
-    const bookshelf = document.querySelector('.books');
+    myLibrary.push({title: form.title.value, author: form.author.value, pages: form.pages.value, isRead: form.isRead.value})
+    console.log(myLibrary[0]);
 }
 
